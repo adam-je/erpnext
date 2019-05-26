f00ee7bda0f021dcb9de576e06b50280c09dbcbc
Adam Dawoodjee <adam.daveed@gmail.com>
2019-05-26 20:35:51 +0200

    add make order method

--- erpnext/restaurant/doctype/restaurant_order_entry/restaurant_order_entry.js
+++ erpnext/restaurant/doctype/restaurant_order_entry/restaurant_order_entry.js
@@ -158,5 +158,82 @@ frappe.ui.form.on('Restaurant Order Entry', {
 		frm.set_value('last_sales_invoice', invoice.name);
 		frm.invoice = invoice;
 		frm.refresh();
+	},
+	make_work_order() {
+		var me = this;
+		this.frm.call({
+			doc: this.frm.doc,
+			method: 'get_work_order_items',
+			callback: function(r) {
+				if(!r.message) {
+					frappe.msgprint({
+						title: __('Work Order not created'),
+						message: __('No Items with Bill of Materials to Manufacture'),
+						indicator: 'orange'
+					});
+					return;
+				}
+				else if(!r.message) {
+					frappe.msgprint({
+						title: __('Work Order not created'),
+						message: __('Work Order already created for all items with BOM'),
+						indicator: 'orange'
+					});
+					return;
+				} else {
+					var fields = [
+						{fieldtype:'Table', fieldname: 'items',
+							description: __('Select BOM and Qty for Production'),
+							fields: [
+								{fieldtype:'Read Only', fieldname:'item_code',
+									label: __('Item Code'), in_list_view:1},
+								{fieldtype:'Link', fieldname:'bom', options: 'BOM', reqd: 1,
+									label: __('Select BOM'), in_list_view:1, get_query: function(doc) {
+										return {filters: {item: doc.item_code}};
+									}},
+								{fieldtype:'Float', fieldname:'pending_qty', reqd: 1,
+									label: __('Qty'), in_list_view:1},
+								{fieldtype:'Data', fieldname:'restaurant_order_item', reqd: 1,
+									label: __('Restaurant Order Item'), hidden:1}
+							],
+							data: r.message,
+							get_data: function() {
+								return r.message
+							}
+						}
+					]
+					var d = new frappe.ui.Dialog({
+						title: __('Select Items to Manufacture'),
+						fields: fields,
+						primary_action: function() {
+							var data = d.get_values();
+							me.frm.call({
+								method: 'make_work_orders',
+								args: {
+									items: data,
+									company: me.frm.doc.company,
+									sales_order: me.frm.docname
+								},
+								freeze: true,
+								callback: function(r) {
+									if(r.message) {
+										frappe.msgprint({
+											message: __('Work Orders Created: {0}',
+												[r.message.map(function(d) {
+													return repl('<a href="#Form/Work Order/%(name)s">%(name)s</a>', {name:d})
+												}).join(', ')]),
+											indicator: 'green'
+										})
+									}
+									d.hide();
+								}
+							});
+						},
+						primary_action_label: __('Create')
+					});
+					d.show();
+				}
+			}
+		});
 	}
 });
