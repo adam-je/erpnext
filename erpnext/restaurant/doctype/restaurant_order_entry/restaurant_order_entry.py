05b1546458ab355c514e2f439ab71e56373adcbb
Adam Dawoodjee <adam.daveed@gmail.com>
2019-05-26 20:53:58 +0200

    add make work order

--- erpnext/restaurant/doctype/restaurant_order_entry/restaurant_order_entry.py
+++ erpnext/restaurant/doctype/restaurant_order_entry/restaurant_order_entry.py
@@ -85,3 +85,31 @@ def get_restaurant_and_menu_name(table):
 		frappe.throw(_('Please set an active menu for Restaurant {0}').format(restaurant))
 
 	return restaurant, menu
+
+def make_work_orders(items, sales_order, company, project=None):
+	'''Make Work Orders against the given Sales Order for the given `items`'''
+	items = json.loads(items).get('items')
+	out = []
+
+	for i in items:
+		if not i.get("bom"):
+			frappe.throw(_("Please select BOM against item {0}").format(i.get("item_code")))
+		if not i.get("pending_qty"):
+			frappe.throw(_("Please select Qty against item {0}").format(i.get("item_code")))
+
+		work_order = frappe.get_doc(dict(
+			doctype='Work Order',
+			production_item=i['item_code'],
+			bom_no=i.get('bom'),
+			qty=i['pending_qty'],
+			company=company,
+			sales_order=sales_order,
+			sales_order_item=i['restaurant_order_item'],
+			fg_warehouse=i['warehouse'],
+			description=i['description']
+		)).insert()
+		work_order.set_work_order_operations()
+		work_order.save()
+		out.append(work_order)
+
+	return [p.name for p in out]
